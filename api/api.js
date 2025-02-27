5234a873912e32978a3fdeeb6b6c8ee9365a925e
idkwhatiamdoingwithmylife <braedeng482@gmail.com>
2025-02-26 19:33:51 -0500

    Update api.js

--- api/api.js
+++ api/api.js
@@ -2,11 +2,10 @@ module.exports = (req, res) => {
   const apiKey = req.query.apiKey || req.headers['x-api-key'];
 
   if (apiKey === '1234') {
-    res.status(200).json({ message: 'Success' });
-
     for (let i = 0; i < 100; i++) {
       console.log('hi');
     }
+    res.status(200).json({ message: 'Success' });
   } else {
     res.status(403).json({ error: 'Forbidden: Invalid API Key' });
   }
